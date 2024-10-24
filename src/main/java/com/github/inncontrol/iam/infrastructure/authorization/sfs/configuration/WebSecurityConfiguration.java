dce951493a14a20a7dd18bba2e01ba3d35e94b51
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2024-10-24 00:28:00 -0500

    Fixed:cors method

--- src/main/java/com/github/inncontrol/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/github/inncontrol/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -65,13 +65,15 @@ public class WebSecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // CORS default configuration
-        http.cors(configurer -> configurer.configurationSource( e    -> {
+        http.cors(configurer -> configurer.configurationSource(request -> {
             var cors = new CorsConfiguration();
             cors.setAllowedOrigins(List.of("*"));
-            cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-            cors.setAllowedHeaders(List.of("*"));
+            cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+            cors.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+            cors.setAllowCredentials(true);  // Permitir credenciales si es necesario
             return cors;
         }));
+
         http.csrf(AbstractHttpConfigurer::disable)
                 .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(unauthorizedRequestHandler))
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
